2f72793016cb266d9739e8fe429239c62e19dd46
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-04-24 10:50:49 +0530

    added memory leek tracking for components in integration

--- EssentialFeeds/EssentialFeedsApiEndToEndTests/EssentialFeedsApiEndToEndTests.swift
+++ EssentialFeeds/EssentialFeedsApiEndToEndTests/EssentialFeedsApiEndToEndTests.swift
@@ -12,8 +12,16 @@ class EssentialFeedsApiEndToEndTests: XCTestCase {
     
     func test_endToEndServerGetFeedResults_matchesFixedTestAccountData(){
         switch getFeedResults() {
-        case let .success(item):
-            XCTAssertEqual(item.count, 8,"total feed item counts is 8")
+        case let .success(items):
+            XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
+            XCTAssertEqual(items[0], expectedItem(at: 0))
+            XCTAssertEqual(items[1], expectedItem(at: 1))
+            XCTAssertEqual(items[2], expectedItem(at: 2))
+            XCTAssertEqual(items[3], expectedItem(at: 3))
+            XCTAssertEqual(items[4], expectedItem(at: 4))
+            XCTAssertEqual(items[5], expectedItem(at: 5))
+            XCTAssertEqual(items[6], expectedItem(at: 6))
+            XCTAssertEqual(items[7], expectedItem(at: 7))
         case let .failure(error):
             XCTFail("expected feed items , but received \(error) instead")
         default:
@@ -21,11 +29,12 @@ class EssentialFeedsApiEndToEndTests: XCTestCase {
         }
     }
     
-    private func getFeedResults() -> LoadFeedResults?{
+    private func getFeedResults(file:StaticString = #filePath, line:UInt = #line) -> LoadFeedResults?{
         let url = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: url, client: client)
-        
+        trackForMemoryLeaks(loader,file:file,line:line)
+        trackForMemoryLeaks(client,file:file,line: line)
         let expectation = expectation(description: "wait for load completion")
         var receivedResult:LoadFeedResults?
         
@@ -36,5 +45,56 @@ class EssentialFeedsApiEndToEndTests: XCTestCase {
         
         wait(for: [expectation], timeout: 15.0)
         return receivedResult
+    }
+    
+    private func expectedItem(at index: Int) -> FeedItems {
+        return FeedItems(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
+            imageURL: imageURL(at: index))
+    }
+    
+    private func id(at index: Int) -> UUID {
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][index])!
+    }
+    
+    private func description(at index: Int) -> String? {
+        return [
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8"
+        ][index]
+    }
+    
+    private func location(at index: Int) -> String? {
+        return [
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8"
+        ][index]
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: "https://url-\(index+1).com")!
     }
 }

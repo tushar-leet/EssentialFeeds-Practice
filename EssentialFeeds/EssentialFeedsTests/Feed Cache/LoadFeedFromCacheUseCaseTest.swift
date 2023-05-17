55842e47314b95c26e0062a8cff16238dd758c1d
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-05-17 09:29:07 +0530

    moved FeedStoreSpy in its own helper class to remove duplication

--- EssentialFeeds/EssentialFeedsTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
+++ EssentialFeeds/EssentialFeedsTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
@@ -21,44 +21,4 @@ final class LoadFeedFromCacheUseCaseTest: XCTestCase {
         trackForMemoryLeaks(store)
         return (sut,store)
     }
-    
-    class FeedStoreSpy:FeedStore{
-        typealias DeletionCompletion = (Error?) -> ()
-        typealias InsertCompletion = (Error?) -> ()
-       
-        enum ReceivedMessages:Equatable{
-            case deleteCachedFeed
-            case insert([LocalFeedImage],Date)
-        }
-        
-        private var deletionCompletion = [DeletionCompletion]()
-        private var insertCompletion = [InsertCompletion]()
-        private(set) var receivedMessages = [ReceivedMessages]()
-
-        func deleteCachedFeed(completion:@escaping DeletionCompletion){
-            deletionCompletion.append(completion)
-            receivedMessages.append(.deleteCachedFeed)
-        }
-
-        func completeDeletion(with error:Error?, at index:Int = 0){
-            deletionCompletion[index](error)
-        }
-
-        func completeDeletionSuccessfully(at index:Int = 0){
-            deletionCompletion[index](nil)
-        }
-
-        func insert(_ feeds:[LocalFeedImage],timestamp:Date,completion:@escaping InsertCompletion){
-            receivedMessages.append(.insert(feeds, timestamp))
-            insertCompletion.append(completion)
-        }
-        
-        func completeInsertion(with error:Error?, at index:Int = 0){
-            insertCompletion[index](error)
-        }
-        
-        func completeInsertionSuccessfully(at index:Int = 0){
-            insertCompletion[index](nil)
-        }
-    }
 }

121db3ff98937148c01f6b0ae5352b55290a5f72
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-05-29 10:20:28 +0530

    CoreDataFeedStore.deleteCachedFeed() empties previously inserted cache

--- EssentialFeeds/EssentialFeeds/FeedCache/CoreDataFeedStore.swift
+++ EssentialFeeds/EssentialFeeds/FeedCache/CoreDataFeedStore.swift
@@ -64,7 +64,15 @@ public final class CoreDataFeedStore:FeedStore{
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        completion(nil)
+        let context = self.context
+        context.perform {
+            do{
+                try ManagedCache.find(in: context).map(context.delete).map(context.save)
+                completion(nil)
+            }catch{
+                completion(error)
+            }
+        }
     }
     
     public func insert(_ feeds: [LocalFeedImage], timestamp: Date, completion: @escaping InsertCompletion) {

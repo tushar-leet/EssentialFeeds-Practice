8c7cbdc6f9e9c7a1b4e6dd6702dd0f6734ddce6f
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-05-29 11:06:19 +0530

    Extract CoreData managed model classes into separate files

--- EssentialFeeds/EssentialFeeds/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeeds/EssentialFeeds/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -5,53 +5,8 @@
 //  Created by TUSHAR SHARMA on 27/05/23.
 //
 
-import Foundation
+
 import CoreData
-
-@objc(ManagedCache)
-public class ManagedCache: NSManagedObject {
-     @NSManaged var timestamp: Date
-     @NSManaged var feed: NSSet
-     
-    var localFeed:[LocalFeedImage]{
-        feed.compactMap{($0 as? ManagedFeedImage)?.local}
-    }
-    
-    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
-        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
-        request.returnsObjectsAsFaults = false
-        return try context.fetch(request).first
-    }
-    
-    static func newUniqueInstance(in context:NSManagedObjectContext) throws -> ManagedCache{
-        try find(in: context).map(context.delete)
-        return ManagedCache(context: context)
-    }
- }
-
-@objc(ManagedFeedImage)
- public  class ManagedFeedImage: NSManagedObject {
-     @NSManaged var id: UUID
-     @NSManaged var imageDescription: String?
-     @NSManaged var location: String?
-     @NSManaged var url: URL
-     @NSManaged var cache: ManagedCache
-     
-     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSSet {
-         return NSSet(array: localFeed.map { local in
-             let managed = ManagedFeedImage(context: context)
-             managed.id = local.id
-             managed.imageDescription = local.description
-             managed.location = local.location
-             managed.url = local.url
-             return managed
-         })
-     }
-     
-     var local:LocalFeedImage{
-         LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
-     }
- }
 
 public final class CoreDataFeedStore:FeedStore{
     

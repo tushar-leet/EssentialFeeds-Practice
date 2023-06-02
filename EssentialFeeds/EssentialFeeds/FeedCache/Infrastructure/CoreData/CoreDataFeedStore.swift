022d0c133ae89acfa2eed7dfabcf54609bdf78ac
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-06-02 16:50:50 +0530

    Load NSManagedObjectModel instance lazily and cache it to prevent multiple `NSEntityDescriptions` claiming the same `NSManagedObject` model subclasses (This problem just generates warnings but could lead to undefined behavior in the future).

--- EssentialFeeds/EssentialFeeds/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeeds/EssentialFeeds/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -10,12 +10,28 @@ import CoreData
 
 public final class CoreDataFeedStore:FeedStore{
     
+    private static let modelName = "FeedStore"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
+    
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
+    
     public init(storeURL: URL, bundle: Bundle = .main) throws {
-        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
-        context = container.newBackgroundContext()
+        guard let model = CoreDataFeedStore.model else {
+            throw StoreError.modelNotFound
+        }
+        
+        do {
+            container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentContainer(error)
+        }
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {

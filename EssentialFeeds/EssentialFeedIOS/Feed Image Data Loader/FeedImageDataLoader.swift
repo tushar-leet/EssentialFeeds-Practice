9b56fcf23138820c63403a6ce16ea4c2017894cc
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-06-26 06:43:25 +0530

    Extract UIRefreshControl creation/configuration and refresh logic with `FeedLoader` to the new `FeedRefreshViewController`

---
+++ EssentialFeeds/EssentialFeedIOS/Feed Image Data Loader/FeedImageDataLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeedIOS
+//
+//  Created by TUSHAR SHARMA on 26/06/23.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask{
+    func cancel()
+}
+
+public protocol FeedImageDataLoader{
+    typealias Result = Swift.Result<Data, Error>
+    func loadImageData(from url:URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}

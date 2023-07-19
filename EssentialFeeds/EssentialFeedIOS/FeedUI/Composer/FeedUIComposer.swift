983041fd4cdacf88f78876e5db7da4cbdf1a79a8
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-07-19 07:40:08 +0530

    Move memory management to Composer layer. The FeedPresenter shouldn have to know or handle their dependencies lifetime.

--- EssentialFeeds/EssentialFeedIOS/FeedUI/Composer/FeedUIComposer.swift
+++ EssentialFeeds/EssentialFeedIOS/FeedUI/Composer/FeedUIComposer.swift
@@ -13,15 +13,28 @@ public final class FeedUIComposer {
 
      public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
          let presenter = FeedPresenter(feedLoader: feedLoader)
-         let viewModel = FeedViewModel(feedLoader: feedLoader)
+        // let viewModel = FeedViewModel(feedLoader: feedLoader)
          let refreshController = FeedRefreshViewController(presenter: presenter)
          let feedController = FeedViewController(refreshController: refreshController)
-         presenter.loadingView = refreshController
+         presenter.loadingView = WeakRefVirtualProxy(object: refreshController)
          presenter.feedView = FeedViewAdapter(controller: feedController,loader: imageLoader)
          return feedController
      }
-
  }
+
+private final class WeakRefVirtualProxy<T:AnyObject>{
+    private weak var object:T?
+    
+    init(object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy:FeedLoadingView where T:FeedLoadingView{
+    func display(isLoading: Bool) {
+        object?.display(isLoading: isLoading)
+    }
+}
 
 private final class FeedViewAdapter:FeedView{
 
--- EssentialFeeds/EssentialFeedIOS/FeedUI/Model/FeedPresenter.swift
+++ EssentialFeeds/EssentialFeedIOS/FeedUI/Model/FeedPresenter.swift
@@ -11,7 +11,7 @@ protocol FeedView{
     func display(feed:[FeedImage])
 }
 
-protocol FeedLoadingView:AnyObject{
+protocol FeedLoadingView{
     func display(isLoading:Bool)
 }
 
@@ -23,7 +23,7 @@ final class FeedPresenter{
         self.feedLoader = feedLoader
     }
     
-    weak var loadingView:FeedLoadingView?
+    var loadingView:FeedLoadingView?
     var feedView:FeedView?
     
     

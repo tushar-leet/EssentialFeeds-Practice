68d42b51b2cdbcff8796bfcd929d72b3a955f63e
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-08-01 10:57:05 +0530

    Move ErrorView to separate file

---
+++ EssentialFeeds/EssentialFeedIOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeedIOS
+//
+//  Created by TUSHAR SHARMA on 01/08/23.
+//
+
+import UIKit
+
+ public final class ErrorView: UIView {
+     public var message: String?
+ }

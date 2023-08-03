a59be97a997a5f82719b22a70aee663a48b47e08
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-08-03 10:05:56 +0530

    Inline Data creation

--- EssentialFeeds/EssentialFeedsTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeeds/EssentialFeedsTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -90,9 +90,8 @@ final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == I
      func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
          let (sut, view) = makeSUT(imageTransformer:fail)
          let image = uniqueImage()
-         let data = Data()
 
-         sut.didFinishLoadingImageData(with: data, for: image)
+         sut.didFinishLoadingImageData(with: Data(), for: image)
 
          let message = view.messages.first
          XCTAssertEqual(view.messages.count, 1)
@@ -105,11 +104,10 @@ final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == I
      
      func test_didFinishLoadingImageData_displaysImageOnSuccessfulTransformation() {
          let image = uniqueImage()
-         let data = Data()
          let transformedData = AnyImage()
          let (sut, view) = makeSUT(imageTransformer: { _ in transformedData })
          
-         sut.didFinishLoadingImageData(with: data, for: image)
+         sut.didFinishLoadingImageData(with: Data(), for: image)
          
          let message = view.messages.first
          XCTAssertEqual(view.messages.count, 1)

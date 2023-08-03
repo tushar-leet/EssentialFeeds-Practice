4b500d9d5252967fecc9df572584f28d3ac6f86e
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-08-03 09:25:03 +0530

    FeedImagePresenter displays loading image on `didStartLoadingImageData`

--- EssentialFeeds/EssentialFeedsTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeeds/EssentialFeedsTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -6,12 +6,40 @@
 //
 
 import XCTest
+import EssentialFeeds
 
-class FeedImagePresenter {
-     init(view: Any) {
+ struct FeedImageViewModel {
+     let description: String?
+     let location: String?
+     let image: Any?
+     let isLoading: Bool
+     let shouldRetry: Bool
 
+     var hasLocation: Bool {
+         return location != nil
      }
  }
+
+ protocol FeedImageView {
+     func display(_ model: FeedImageViewModel)
+ }
+
+class FeedImagePresenter {
+    private let view: FeedImageView
+    
+    init(view: FeedImageView) {
+        self.view = view
+    }
+    
+    func didStartLoadingImageData(for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: true,
+            shouldRetry: false))
+    }
+}
 
  class FeedImagePresenterTests: XCTestCase {
 
@@ -21,6 +49,20 @@ class FeedImagePresenter {
          XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
      }
 
+     func test_didStartLoadingImageData_displaysLoadingImage() {
+         let (sut, view) = makeSUT()
+         let image = uniqueImage()
+         
+         sut.didStartLoadingImageData(for: image)
+         
+         let message = view.messages.first
+         XCTAssertEqual(view.messages.count, 1)
+         XCTAssertEqual(message?.description, image.description)
+         XCTAssertEqual(message?.location, image.location)
+         XCTAssertEqual(message?.isLoading, true)
+         XCTAssertEqual(message?.shouldRetry, false)
+         XCTAssertNil(message?.image)
+     }
      // MARK: - Helpers
      
      private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
@@ -31,8 +73,15 @@ class FeedImagePresenter {
          return (sut, view)
      }
 
-     private class ViewSpy {
-         let messages = [Any]()
+     private class ViewSpy: FeedImageView {
+         private(set) var messages = [FeedImageViewModel]()
+         
+         func display(_ model: FeedImageViewModel) {
+             messages.append(model)
+         }
      }
 
+     func uniqueImage() -> FeedImage{
+         return FeedImage(id: UUID(), description: "Any", location: "Any", url: URL(string: "HTTPS")!)
+     }
  }

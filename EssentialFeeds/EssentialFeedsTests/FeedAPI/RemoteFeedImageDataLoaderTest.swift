0ee8e68602cbcbb0622f4aa169548b4226c431bb
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-08-04 17:58:56 +0530

    RemoteFeedImageDataLoader.loadImageDataFromURL delivers non-empty received data on 200 HTTP response

--- EssentialFeeds/EssentialFeedsTests/FeedAPI/RemoteFeedImageDataLoaderTest.swift
+++ EssentialFeeds/EssentialFeedsTests/FeedAPI/RemoteFeedImageDataLoaderTest.swift
@@ -22,7 +22,12 @@ class RemoteFeedImageDataLoader {
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .success: completion(.failure(Error.invalidData))
+            case let .success(data, response):
+                if response.statusCode == 200, !data.isEmpty {
+                    completion(.success(data))
+                } else {
+                    completion(.failure(Error.invalidData))
+                }
             case let .failure(error): completion(.failure(error))
             }
         }
@@ -87,6 +92,15 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         }
     }
 
+    func test_loadImageDataFromURL_deliversReceivedNonEmptyDataOn200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        let nonEmptyData = Data("non-empty data".utf8)
+        
+        expect(sut, toCompleteWith: .success(nonEmptyData), when: {
+            client.complete(withStatusCode: 200, data: nonEmptyData)
+        })
+    }
+    
     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedImageDataLoader(client: client)

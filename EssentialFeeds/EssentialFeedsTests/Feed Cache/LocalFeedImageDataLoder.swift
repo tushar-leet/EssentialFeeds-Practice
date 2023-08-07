17612d5dbef750657cc1d9c551560ce5e49551e9
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-08-07 12:28:43 +0530

    LocalFeedImageDataLoader does not message store upon creation

---
+++ EssentialFeeds/EssentialFeedsTests/Feed Cache/LocalFeedImageDataLoder.swift
@@ -0,0 +1,39 @@
+//
+//  LocalFeedImageDataLoder.swift
+//  EssentialFeedsTests
+//
+//  Created by TUSHAR SHARMA on 07/08/23.
+//
+
+import XCTest
+import EssentialFeeds
+
+ final class LocalFeedImageDataLoader {
+     init(store: Any) {
+
+     }
+ }
+
+ class LocalFeedImageDataLoaderTests: XCTestCase {
+
+     func test_init_doesNotMessageStoreUponCreation() {
+         let (_, store) = makeSUT()
+
+         XCTAssertTrue(store.receivedMessages.isEmpty)
+     }
+
+     // MARK: - Helpers
+     
+     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
+         let store = FeedStoreSpy()
+         let sut = LocalFeedImageDataLoader(store: store)
+         trackForMemoryLeaks(store, file: file, line: line)
+         trackForMemoryLeaks(sut, file: file, line: line)
+         return (sut, store)
+     }
+
+     private class FeedStoreSpy {
+         let receivedMessages = [Any]()
+     }
+
+ }

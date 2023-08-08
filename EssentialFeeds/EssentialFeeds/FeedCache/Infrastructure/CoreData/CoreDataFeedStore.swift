5ae0e11e6d3ad05576994b6a5434ed8fba67bfaf
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-08-08 10:50:33 +0530

    Move CoreDataFeedStore implementation of the FeedStore protocol t… …o separate file

--- EssentialFeeds/EssentialFeeds/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeeds/EssentialFeeds/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -8,7 +8,7 @@
 
 import CoreData
 
-public final class CoreDataFeedStore:FeedStore{
+public final class CoreDataFeedStore{
     
     private static let modelName = "FeedStore"
     private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
@@ -31,37 +31,6 @@ public final class CoreDataFeedStore:FeedStore{
             context = container.newBackgroundContext()
         } catch {
             throw StoreError.failedToLoadPersistentContainer(error)
-        }
-    }
-    
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in
-            completion(Result(catching: {
-                try ManagedCache.find(in: context).map(context.delete).map(context.save)
-            }))
-        }
-    }
-    
-    public func insert(_ feeds: [LocalFeedImage], timestamp: Date, completion: @escaping InsertCompletion) {
-        perform { context in
-            completion(Result(catching: {
-                let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                managedCache.timestamp = timestamp
-                managedCache.feed = ManagedFeedImage.images(from: feeds, in: context)
-                try context.save()
-            }))
-        }
-    }
-    
-    public func retrieve(completion: @escaping RetrieveCompletion) {
-        perform { context in
-            completion(Result{
-                try ManagedCache.find(in: context).map  {
-                     CacheFeed(
-                        feed: $0.localFeed,
-                        timestamp: $0.timestamp)
-                }
-            })
         }
     }
     

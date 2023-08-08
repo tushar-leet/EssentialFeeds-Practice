1b7315747e719b68a7f3d4da430b7de6c4e2788c
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-08-08 10:43:51 +0530

    Refactor procedural code into a chain of map operations

--- EssentialFeeds/EssentialFeeds/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
+++ EssentialFeeds/EssentialFeeds/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -13,9 +13,9 @@ import Foundation
      public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
          perform { context in
              completion(Result {
-                 let image = try ManagedFeedImage.first(with: url, in: context)
-                 image?.data = data
-                 try context.save()
+                 try ManagedFeedImage.first(with: url, in: context)
+                                      .map { $0.data = data }
+                                      .map(context.save)
              })
          }
      }

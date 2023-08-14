38b33dbb510d255a12aab45197e1ebd5e63913f3
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-08-14 09:59:16 +0530

    Displays cached feed on launch when customer has no connectivity

--- EssentialApp/EssentialAppUIAcceptenceTests/EssentialAppUIAcceptenceTests.swift
+++ EssentialApp/EssentialAppUIAcceptenceTests/EssentialAppUIAcceptenceTests.swift
@@ -16,5 +16,23 @@ final class EssentialAppUIAcceptenceTests: XCTestCase {
         let feedImages = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(feedImages.exists)
     }
+    
+    
+    func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
+        let onlineApp = XCUIApplication()
+        onlineApp.launch()
+        
+        let offlineApp = XCUIApplication()
+        offlineApp.launchArguments = ["-connectivity", "offline"]
+        offlineApp.launch()
+        
+        let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(cachedFeedCells.count, 22)
+        
+        let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstCachedImage.exists)
+        
+    }
+    
 }
 

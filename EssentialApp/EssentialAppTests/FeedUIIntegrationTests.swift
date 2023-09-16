b49af80304c908f7f9c681e40be86e4f98cdb132
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-09-16 19:35:48 +0530

    Move "GENERIC_CONNECTION_ERROR" localization key to new Shared.strings

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -18,7 +18,7 @@ final class FeedUIIntegrationTests: XCTestCase {
         
         sut.loadViewIfNeeded()
         
-        XCTAssertEqual(sut.title, localized("FEED_VIEW_TITLE"))
+        XCTAssertEqual(sut.title,feedTitle)
     }
 
     func test_loadFeedActions_requiredFeedFromLoader(){
@@ -309,7 +309,7 @@ final class FeedUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.errorMessage, nil)
         
         loader.completeFeedLoadingWithError(at: 0)
-        XCTAssertEqual(sut.errorMessage, localized("GENERIC_CONNECTION_ERROR"))
+        XCTAssertEqual(sut.errorMessage, loadError)
         sut.simulateUserInitatedFeedReload()
         XCTAssertEqual(sut.errorMessage, nil)
     }

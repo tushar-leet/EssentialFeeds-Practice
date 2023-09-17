c53a1ff218577fe022ecc2c98ea3749e6add1cd9
“tushar-leet” <tushar.sharmaleet@gmail.com>
2023-09-17 23:17:34 +0530

    Inject calendar to make tests reliable

--- EssentialFeeds/EssentialFeedsTests/Helper/FeedCacheTestHelpers.swift
+++ EssentialFeeds/EssentialFeedsTests/Helper/FeedCacheTestHelpers.swift
@@ -33,11 +33,11 @@ extension Date{
         self + seconds
     }
     
-    func adding(days:Int) -> Date{
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    func adding(days:Int,calendar: Calendar = Calendar(identifier: .gregorian)) -> Date{
+        return calendar.date(byAdding: .day, value: days, to: self)!
     }
     
-    func adding(minutes:Int) -> Date{
-        return Calendar(identifier: .gregorian).date(byAdding: .minute, value: minutes, to: self)!
+    func adding(minutes:Int,calendar: Calendar = Calendar(identifier: .gregorian)) -> Date{
+        return calendar.date(byAdding: .minute, value: minutes, to: self)!
     }
 }
